05ffd16dda7d3df7b7d2c8dba37182403629b2d9
Agustin Recouso <agustin.recouso@nextroll.com>
2023-09-22 00:51:17 +0200

    redirect to pdf file

--- src/pages/showcase.js
+++ src/pages/showcase.js
@@ -3,8 +3,14 @@ import styled from 'styled-components'
 import {styles} from '../content/content.json'
 
 
-const Showcase = () => {
+const Showcase = ({path}) => {
+  let cleanPath = path.endsWith('/') ? path.slice(0, -1) : path;
+
   const pdfUrl = "/showcase.pdf"; 
+  if (typeof window !== `undefined`) {
+    // redirect if is window is available (client)
+    window.location.href = window.location.href.replace(cleanPath, pdfUrl)
+  }
 
   const ShowcaseMainContainer = styled.div`
     padding-bottom: 151px;
